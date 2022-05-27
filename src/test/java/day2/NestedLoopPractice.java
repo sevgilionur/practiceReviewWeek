a94dab0c5c758f3a405246ecb86092275b345110
onur <sevgilionur25@hotmail.com>
2022-05-27 11:22:25 +0300

    Review Week: Day2

---
+++ src/test/java/day2/NestedLoopPractice.java
@@ -0,0 +1,53 @@
+package day2;
+
+import java.util.Collections;
+
+public class NestedLoopPractice {
+
+    public static void main(String[] args) {
+
+        String str = "aabcccd";
+
+        String result = "";
+
+        for (int j = 0; j < str.length(); j++) {
+
+            char ch = str.charAt(j); //each char of str
+            int count = 0;
+
+            for (int i = 0; i < str.length() ; i++) {
+
+                char each = str.charAt(i);
+
+                if(ch==each){
+
+                    count++;
+                }
+
+
+            }
+
+            if(result.contains(""+ch)){
+                continue;
+            }
+
+            result+=ch;
+            result+=count;
+
+        }
+
+        System.out.println(result);
+
+
+
+    }
+
+}
+
+/*
+ Write a program that can find the frequency of the characters from a string
+			 Ex:
+                        str = "aabcccd";
+                        output:
+                                a2b1c3d1
+ */
---
+++ src/test/java/day2/WhileLoopPractice.java
@@ -0,0 +1,28 @@
+package day2;
+
+public class WhileLoopPractice {
+
+}
+/*
+    1-Ask user to enter a number
+    if it is odd, print ""Odd Number";
+    if it is even, print "Even Number"
+
+    Ask user again if wants to continue or not, if answer is "yes", continue to enter number,
+                                                if answer is "no", send "Thank you!" message and close the program.
+
+    If answer is not "yes" or "no" Don't let user continue and close the program with "Invalid Entry!" message
+
+
+
+
+    2-You are writing a code for the log-in function of the Cydeo Application, assume that your credentials are:
+                    username: Cydeo
+                    password: Cydeo123
+        Ask the user to enter their credentials.
+                If credentials are matched, your program should print "Logged in."
+                If the credentials are not matched, the program should allow the user to have three attempts
+                to enter correct credentials and if all three attempts are failed, then print "Your account is locked."
+ */
+
+
