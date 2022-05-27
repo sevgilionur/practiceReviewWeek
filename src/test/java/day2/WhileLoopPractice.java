8ce1a23a2d821d7fb59830ebcc9799562f94d251
onur <sevgilionur25@hotmail.com>
2022-05-27 11:38:07 +0300

    Review Week: Day2

--- src/test/java/day2/WhileLoopPractice.java
+++ src/test/java/day2/WhileLoopPractice.java
@@ -1,6 +1,42 @@
 package day2;
 
+import java.util.Scanner;
+
 public class WhileLoopPractice {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        while (true){
+
+            System.out.println("Please enter a number");
+            int num = scan.nextInt();
+
+            if(num%2==0){
+                System.out.println("Even number");
+            }else{
+                System.out.println("Odd number");
+            }
+
+            System.out.println("Would you like to enter another number. (yes/no)");
+            String answer = scan.next();
+
+            if(  !(answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("no"))){
+                System.err.println("Invalid entry!");
+                System.exit(1);
+            }
+
+            if(answer.equalsIgnoreCase("no")){
+
+                System.out.println("Thank you!");
+                break;
+
+            }
+        }
+
+
+    }
 
 }
 /*
