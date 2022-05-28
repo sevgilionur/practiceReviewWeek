c3022939c7c8af3900c6c6549d09b59e2a9df44f
onur <sevgilionur25@hotmail.com>
2022-05-28 12:06:12 +0300

    Review Week: Day3

--- src/test/java/day3/ArrayPractice2.java
+++ src/test/java/day3/ArrayPractice2.java
@@ -1,6 +1,54 @@
 package day3;
 
+import java.util.Arrays;
+
 public class ArrayPractice2 {
+
+    public static void main(String[] args) {
+
+        String[] words = {"Aygun", "Aygun", "Oleksandr", "Olga" ,"Adam", "Adam", "Adam", "Cihad", "Cihad", "Max"};
+
+        for (String element : words) {
+
+            int frequency = 0;
+
+            for (String each : words) {
+
+                if(each.equals(element)){
+
+                    frequency++;
+                }
+            }
+
+            if(frequency == 1){
+                System.out.println(element);
+            }
+        }
+
+        System.out.println("----------------------------------------");
+
+        String[] group1 = {"Ali", "Layan", "Ayşenur"};
+        String[] group2 = {"Maria", "Aygun", "Duygu", "Suat", "Valeriy"};
+
+        String[] result = new String[group1.length + group2.length];
+
+        int i = 0;
+
+        for (String each : group1) {
+
+            result[i++] = each;
+
+        }
+
+        for (String each : group2) {
+
+            result[i++]=each;
+
+        }
+
+        System.out.println(Arrays.toString(result));
+
+    }
 }
 
 /*
