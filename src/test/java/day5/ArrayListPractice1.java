ba95e7410d643175b55fb11b5757e9464f6f6763
onur <sevgilionur25@hotmail.com>
2022-05-31 11:36:09 +0300

    Review Week: Day5

--- src/test/java/day5/ArrayListPractice1.java
+++ src/test/java/day5/ArrayListPractice1.java
@@ -1,6 +1,112 @@
 package day5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ArrayListPractice1 {
+
+    public static void main(String[] args) {
+
+        String str = "aaabbbcdddfeee";
+
+        String[] arr = str.split("");
+        //char[] chr = str.toCharArray();
+
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(arr));
+
+        String result = "";
+
+        for (String each : list) {
+
+            int frequency = Collections.frequency(list,each);
+
+            if(frequency==1){
+                result+=each;
+            }
+
+        }
+
+        /**
+        for (char each : chr) {
+            list.add(""+each);
+        }
+
+         */
+
+        System.out.println(list);
+        System.out.println(result);
+
+        System.out.println("------------------------------------------");
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(10);
+        list2.add(2);
+        list2.add(35);
+        list2.add(42);
+        list2.add(5);
+        list2.add(65);
+        list2.add(7);
+        list2.add(8);
+        list2.add(9);
+
+        int max = list2.get(8); //arr[0]
+        int min = list2.get(8);
+        int sum = 0;
+
+        for (Integer each : list2) {
+
+            if(each > max){
+                max=each;
+            }
+
+            if(each < min){
+                min=each;
+
+            }
+
+            sum+=each;
+
+        }
+
+        System.out.println(max);
+        System.out.println(min);
+        System.out.println(sum);
+
+        System.out.println(sum/list2.size());
+
+
+        System.out.println("-------------------------------------------");
+
+
+        ArrayList<Integer> list3 = new ArrayList<>();
+        list3.add(10);
+        list3.add(10);
+        list3.add(10);
+        list3.add(20);
+        list3.add(20);
+        list3.add(20);
+        list3.add(30);
+        list3.add(30);
+        list3.add(30);
+
+        ArrayList<Integer> result2 = new ArrayList<>();
+
+        for (Integer each : list3) {
+
+            if(!result2.contains(each)){
+
+                result2.add(each);
+
+            }
+        }
+
+        System.out.println(result2);
+
+
+
+
+    }
 }
 /*
       1- Write a program that can only writes unique chars from the given string
