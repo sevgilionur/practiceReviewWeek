6a4f1a4bd5320dbc76a8815625bd826d01441b3b
onur <sevgilionur25@hotmail.com>
2022-05-31 12:49:34 +0300

    Review Week: Day5

--- src/test/java/day5/ArrayListPractice2.java
+++ src/test/java/day5/ArrayListPractice2.java
@@ -1,7 +1,113 @@
 package day5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+
 public class ArrayListPractice2 {
-}
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("Java");
+        list.add("Java");
+        list.add("Python");
+        list.add("C#");
+        list.add("C#");
+        list.add("C#");
+        list.add("Ruby");
+        list.add("C++");
+        list.add("C++");
+
+        ArrayList<String> unique = new ArrayList<>();
+
+        for (String each : list) {
+
+            if(list.indexOf(each)==list.lastIndexOf(each)){
+
+                unique.add(each);
+
+            }
+
+        }
+
+        System.out.println(unique);
+
+        System.out.println("-----------------------------------");
+
+        ArrayList<Character> list2 = new ArrayList<>();
+
+        list2.addAll(Arrays.asList('A', 'A', 'B', 'C', 'C', 'C', 'D', 'E', 'E','F'));
+
+        for (Character element : list2) {
+
+            int frequency = 0;
+            for (Character each : list2) {
+
+                if(each== element){
+
+                    frequency++;
+                }
+            }
+            if(frequency==1){
+                System.out.println(element);
+                break;
+            }
+        }
+
+        System.out.println("---------------------------------------");
+
+        ArrayList<Integer> list3 = new ArrayList<>();
+        list3.addAll(Arrays.asList(1,2,3,4,5,6,7,7,8,8));
+
+        Collections.max(list3);
+
+        int n = 3;
+
+        for (int i = 1; i < n  ; i++) {
+
+            list3.removeIf(p -> Collections.max(list3)==p);
+        }
+
+        int max = Collections.max(list3);
+
+        System.out.println(max);
+
+        System.out.println("------------------------------");
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
 /*
   1-Write a program that can print the unique elements from the given ArrayList with new ArrayList
 
